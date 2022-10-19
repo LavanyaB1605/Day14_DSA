335ef9ca88749e1e3488fc0f752366bfe30b26db
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-10-19 17:36:16 +0530

    first commit

---
+++ src/LlMain.java
@@ -0,0 +1,13 @@
+public class LlMain {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertFirst(70);
+        list.insertFirst(30);
+        list.insertFirst(56);
+
+        list.display();
+
+    }
+}
+
